6c92e4a2562dc4bbdad8523e4e0cfa7485a4f139
Nabal22 <alban.talagrand@gmail.com>
2021-12-15 18:07:17 +0100

    Commande 3 Valide Créer des fichier in pour être sur

--- PartieGagne.cpp
+++ PartieGagne.cpp
@@ -25,11 +25,9 @@ bool estGagne (Grille grille, Conteneur mines){
 		if ( lireGrille(grille, i) == ' ' || lireGrille(grille, i) == '1' || lireGrille(grille, i) == '2' || lireGrille(grille, i) == '3' || lireGrille(grille, i) == '4' 
 			|| lireGrille(grille, i) == '5'|| lireGrille(grille, i) == '6' || lireGrille(grille, i) == '7' || lireGrille(grille, i) == '8') {
 			partieGagne = true;
-			cout << i << " - " << lireGrille(grille, i) << endl;
 		}
 		else if (lireGrille(grille, i) == '.' && verifCase(i,mines) )
 		{
-			cout << "mine en : " << i << endl;
 			partieGagne = true;
 		}
 		else {
